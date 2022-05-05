50e0bcdb1b1df3904c545e7ab0745470f87620a9
shyne <shyne@DESKTOP-HC3OJGB>
2022-05-05 18:51:30 +0200

    Version 2.0

--- src/jararepogit/JaraRepoGit.java
+++ src/jararepogit/JaraRepoGit.java
@@ -15,7 +15,7 @@ public class JaraRepoGit {
      */
     public static void main(String[] args) {
         System.out.println("Matias Jara");
-        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
         
     }
     
